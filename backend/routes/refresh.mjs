3b80e858f7fd301af6f7320a67d9bc1d0f7c2ca5
JayJay-rgb <pjayyyyy@gmail.com>
2026-08-16 21:01:58 +0100

    refresh

--- backend/routes/refresh.mjs
+++ backend/routes/refresh.mjs
@@ -1,7 +1,7 @@
 import express from "express"
 import refreshController from "../controllers/refreshController.mjs"
-
+import verifyJwt from "../middleware/verifyJwt.mjs"
 const refreshRouter = express.Router();
 
-refreshRouter.post("/refresh",refreshController)
+refreshRouter.post("/refresh", verifyJwt, refreshController)
 export default refreshRouter;
